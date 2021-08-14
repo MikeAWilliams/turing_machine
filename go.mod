8050db240fdc1317e3894e2e2852a22ff8430290
Mike Williams <mumakin@gmail.com>
2021-08-14 17:15:51 -0600

    add more state to main

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/MikeAWilliams/turing_machine
 
 go 1.14
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/stretchr/testify v1.7.0
+)
--- main.go
+++ main.go
@@ -4,20 +4,28 @@ import (
 	"fmt"
 
 	"github.com/MikeAWilliams/turing_machine/machine"
+	"github.com/fatih/color"
 )
 
-func main() {
-	fmt.Println("Hello Mr. Turing")
-
-	machine := machine.TuringMachine1()
-
+func execute100Operations(machine machine.Machine) {
 	for i := 0; i < 100; i++ {
-		fmt.Printf("%v -%v-\n", i, machine.TapeAsString())
+		state := machine.StateReport()
+		fmt.Printf("%v -%v", i, string(state.Squares[:state.SquareIndex]))
+		redText := color.New(color.FgRed, color.Bold)
+		redText.Printf("|%v|", string(state.Squares[state.SquareIndex:state.SquareIndex+1]))
+		fmt.Printf("%v- %v %v %v \n", string(state.Squares[state.SquareIndex+1:]), state.CurrentConfiguration, state.OperationRow, state.OperationColumn)
+
 		tmpMachine, err := machine.Operate()
 		if nil != err {
 			panic(err)
 		}
 		machine = tmpMachine
 	}
+}
 
+func main() {
+	fmt.Println("Hello Dr. Turing")
+
+	machine := machine.TuringMachine2()
+	execute100Operations(machine)
 }
