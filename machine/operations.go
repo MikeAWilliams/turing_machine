7c8b413c05e4b01b5d3c9d8d9a8c648a1c5caebf
Mike Williams <mumakin@gmail.com>
2021-08-14 13:01:29 -0600

    operations

---
+++ machine/operations.go
@@ -0,0 +1,44 @@
+package machine
+
+import "errors"
+
+type Operation func(Tape) Tape
+
+type Operations struct {
+	operations       []Operation
+	currentOperation int
+}
+
+func (o Operations) copy() Operations {
+	newOperations := make([]Operation, len(o.operations))
+	copy(newOperations, o.operations)
+	return Operations{operations: newOperations, currentOperation: o.currentOperation}
+}
+
+func (o Operations) Operate(t Tape) (Operations, Tape, error) {
+	if len(o.operations) <= o.currentOperation {
+		return Operations{}, Tape{}, errors.New("operation index out of bounds")
+	}
+	resultTape := o.operations[o.currentOperation](t)
+	resultOperations := o.copy()
+	resultOperations.currentOperation++
+	return resultOperations, resultTape, nil
+}
+
+func NewOperations(op []Operation) Operations {
+	return Operations{operations: op}
+}
+
+func Right(t Tape) Tape {
+	return t.Right()
+}
+
+func Left(t Tape) Tape {
+	return t.Left()
+}
+
+func Print(symbol rune) func(Tape) Tape {
+	return func(t Tape) Tape {
+		return t.Print(symbol)
+	}
+}
---
+++ machine/operations_test.go
@@ -0,0 +1,40 @@
+package machine_test
+
+import (
+	"github.com/MikeAWilliams/turing_machine/machine"
+
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_PrintZeroOne(t *testing.T) {
+	operations := machine.NewOperations([]machine.Operation{machine.Print('0'), machine.Right, machine.Print('1')})
+	tape := machine.NewTape()
+
+	doATest := func() {
+		newOperations, newTape, err := operations.Operate(tape)
+		require.NoError(t, err)
+		require.Equal(t, '0', newTape.GetSymbol())
+
+		newOperations, newTape, err = newOperations.Operate(newTape)
+		require.NoError(t, err)
+		require.Equal(t, ' ', newTape.GetSymbol())
+
+		newOperations, newTape, err = newOperations.Operate(newTape)
+		require.NoError(t, err)
+		require.Equal(t, '1', newTape.GetSymbol())
+	}
+	doATest()
+	doATest()
+}
+
+func Test_OperateToManyTimes(t *testing.T) {
+	operations := machine.NewOperations([]machine.Operation{machine.Print('0')})
+	tape := machine.NewTape()
+
+	operations, tape, err := operations.Operate(tape)
+	require.NoError(t, err)
+	operations, tape, err = operations.Operate(tape)
+	require.Error(t, err)
+}
