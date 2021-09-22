f1225c714564f3a73d0e8433bfbac19709a15983
Mike Williams <mwilliams@alteryx.com>
2021-09-22 10:11:56 -0600

    first parsing of operations

--- parse/operations.go
+++ parse/operations.go
@@ -1,9 +1,17 @@
 package parse
 
 import (
+	"errors"
+
 	"github.com/MikeAWilliams/turing_machine/machine"
 )
 
 func parseOperation(op string) (machine.Operation, error) {
-	return machine.Right, nil
+	if "R" == op {
+		return machine.Right, nil
+	}
+	if "L" == op {
+		return machine.Left, nil
+	}
+	return nil, errors.New("Not implmemented")
 }
--- parse/operations_test.go
+++ parse/operations_test.go
@@ -11,10 +11,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_ParseOpreration(t *testing.T) {
+func Test_ParseOpreration_RightLeft(t *testing.T) {
 	op, err := parseOperation("R")
 	require.NoError(t, err)
 	require.Equal(t, getOperationName(machine.Right), getOperationName(op))
+
+	op, err = parseOperation("L")
+	require.NoError(t, err)
+	require.Equal(t, getOperationName(machine.Left), getOperationName(op))
+}
+
+func Test_ParseOpreration_Invalid(t *testing.T) {
+	_, err := parseOperation("NotValidOperation")
+	require.Error(t, err)
 }
 
 func getOperationName(operation machine.Operation) string {
